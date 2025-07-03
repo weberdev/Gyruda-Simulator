49b2c3eb4b77239b59e49066ef89a11e897aee0e
Ian Weber <iacweber@gmail.com>
2025-07-02 22:59:24 -0700

    Clones added.

--- webscript.js
+++ webscript.js
@@ -3,5 +3,10 @@ type CardType = "LegendaryClone" | "NonLegendaryClone" | "Land" | "Chaff"
 type Deck = CardType[]
 
 function generateDeck(): Deck {
-
+    for (let i = 0; i < 14; i++) {
+        deck.push("LegendaryClone");
+    }
+    for (let i = 0; i < 16; i++) {
+        deck.push("NonLegendaryClone");
+    }
 }
